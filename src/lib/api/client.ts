10086218eb892336536e0e4d5ed8799d070b0b35
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-03-17 16:34:16 -0400

    asdf

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,11 +1,9 @@
 import { hc } from "hono/client";
 import type { AppType } from ".";
 
-const baseUrl = import.meta.env.VERCEL_URL
-  ? `https://${import.meta.env.VERCEL_URL}/`
+const baseUrl = import.meta.env.PUBLIC_VERCEL_URL
+  ? `https://${import.meta.env.PUBLIC_VERCEL_URL}/`
   : "http://localhost:4321/";
-
-console.log(process.env.VERCEL_URL + "/");
 
 const { api } = hc<AppType>(baseUrl);
 
