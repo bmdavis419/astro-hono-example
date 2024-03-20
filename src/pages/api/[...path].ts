ce96a8a967056228134cdd6985bd6242ead12abe
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-03-19 23:57:24 -0400

    updated handler

--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -3,14 +3,4 @@ import type { APIRoute } from "astro";
 
 const handler: APIRoute = ({ request }) => app.fetch(request);
 
-export {
-  handler as GET,
-  handler as POST,
-  handler as PUT,
-  handler as DELETE,
-  handler as HEAD,
-  handler as PATCH,
-  handler as TRACE,
-  handler as OPTIONS,
-  handler as CONNECT,
-};
+export { handler as ALL };
